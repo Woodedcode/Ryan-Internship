00e812f36a1786d39bc9fd7235110da1ceadfa59
Ryan Woods <Woodedcode@gmail.com>
2026-09-09 00:21:51 -0500

    build dynamic hot collections carousel with skeleton loading

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios"
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const settings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows:true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+};
 
 const HotCollections = () => {
+const [collections, setCollections] = useState([]);
+const [loading, setLoading] = useState(true)
+
+useEffect(() => {
+  axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections")
+  .then((response) => {
+    setCollections(response.data)
+    setLoading(false);
+  })
+}, [])
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +60,38 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {loading ? (
+            <div className="hot-collections-skeleton">
+              {new Array(4).fill(0).map((_, index) => (
+                <div className="skeleton-card" key={index}></div>
+              ))}
+            </div> ) : (
+          <Slider {...settings}>
+          {collections.map((collection) => (
+            <div key={collection.nftId}>
               <div className="nft_coll">
                 <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                  <Link to={`/item-details/${collection.nftId}`}>
+                    <img src={collection.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                  <Link to={`/author/${collection.authorId}`} >
+                    <img className="lazy pp-coll" src={collection.authorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{collection.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>ERC-{collection.code}</span>
                 </div>
               </div>
             </div>
           ))}
+        </Slider>
+            )}
         </div>
       </div>
     </section>
